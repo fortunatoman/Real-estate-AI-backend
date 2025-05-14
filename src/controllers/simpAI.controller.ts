e69adce70a2bad9159d678bd140583a66fc46851
fortunatoman <legvfq116@gmx.us>
2025-05-14 03:00:18 -0700

    fixed: fixed home view prompt and added street view route

--- src/controllers/simpAI.controller.ts
+++ src/controllers/simpAI.controller.ts
@@ -1,24 +1,28 @@
 import { Request, Response } from 'express';
 import { extractDescription, extractSearchQuery } from '../lib/prompt';
-import { searchZillow } from '../lib/zillow';
+import { getMarketData, searchZillow } from '../lib/zillow';
 import querystring from 'querystring';
 import axios from 'axios';
+import stateCityMap from '../lib/state';
 
 export const analyzeProperty = async (req: Request, res: Response) => {
     try {
         const { userInput } = req.body;
 
         const query = await extractSearchQuery(userInput);
+
         const jsonCleaned = query?.replace(/```json|```/g, "");
 
         const searchQueryState = JSON.parse(jsonCleaned || '{}');
 
         const encoded = await querystring.escape(JSON.stringify(searchQueryState));
-        const url = `https://www.zillow.com/homes/for_sale/LOCATION_rb/?searchQueryState=${encoded}`;
 
+        const url = `https://www.zillow.com/homes/for_sale/LOCATION_rb/?searchQueryState=${encoded}`;
+        console.log(url);
         const results = await searchZillow(url);
 
         const description = await extractDescription(results);
+
         const described = description?.replace(/\n/g, '<br />').replace(/[#*]/g, '');
 
         if (!results || results.length === 0) {
--- src/lib/zillow.ts
+++ src/lib/zillow.ts
@@ -1,20 +1,54 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
+import stateCityMap from './state';
+import stateAbbreviations from './shortState';
 dotenv.config();
 
 export const searchZillow = async (url: string) => {
-    const response = await axios.get(`https://zillow56.p.rapidapi.com/search_url`, {
-        headers: {
-            'x-rapidapi-key': process.env.RAPIDAPI_API_KEY,
-            'x-rapidapi-host': 'zillow56.p.rapidapi.com'
-        },
-        params: {
-            url: url,
-            page: '1',
-            output: 'json',
-            listing_type: 'by_agent'
-        },
-    });
+    try {
+        const response = await axios.get(`https://zillow56.p.rapidapi.com/search_url`, {
+            headers: {
+                'x-rapidapi-key': process.env.RAPIDAPI_API_KEY,
+                'x-rapidapi-host': 'zillow56.p.rapidapi.com'
+            },
+            params: {
+                url: url,
+                page: '1',
+                output: 'json',
+                listing_type: 'by_agent'
+            },
+        });
 
-    return response.data.results;
+        return response.data.results;
+    } catch (error) {
+        console.error('Error searching Zillow:', error);
+        return null;
+    }
+};
+
+
+export const getMarketData = async (state: string) => {
+    try {
+        let marketData = [];
+        // const stateAbbreviation = stateAbbreviations[state as keyof typeof stateAbbreviations];
+        // const cities = stateCityMap[state as keyof typeof stateCityMap];
+        // for (let city of cities) {
+
+        const response = await axios.get(`https://zillow56.p.rapidapi.com/market_sale_overview`, {
+            params: {
+                location: 'houston,tx'
+            },
+            headers: {
+                'x-rapidapi-key': "2e5dd68fe7mshd661c7de69087c4p153130jsn820b372673ae",
+                'x-rapidapi-host': 'zillow56.p.rapidapi.com'
+            },
+        });
+        // marketData.push(response.data);
+        // }
+        console.log(response);
+        return;
+    } catch (error) {
+        console.error('Error getting market data:', error);
+        return null;
+    }
 };
